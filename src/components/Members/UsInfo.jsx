e4759c8df24818cc64fe436cd65bf146b21f188b
camilo1912 <camilomarilafm@gmail.com>
2023-11-08 20:14:49 -0300

    Creada pagina de datos de la junta de vecinos

--- src/components/Members/UsInfo.jsx
+++ src/components/Members/UsInfo.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../../contexts/UserContext';
+import { get_neighborhood_by_id } from '../../requests/Neighborhood';
+import { formatearFecha } from '../../utils/utils';
 
 const UsInfo = () => {
+    const { userInfo, handleUserInfo } = useContext(UserContext);
+    const neighborhoodId = userInfo.neighborhood.neighborhood_id;
+    const [neighborhoodData, setNeighborhoodData] = useState(null);
+
+    useEffect(() => {
+        if (!neighborhoodData) {
+            const getNeighborhood = async () => {
+                const neighborhoodResponse = await get_neighborhood_by_id(neighborhoodId);
+                setNeighborhoodData(neighborhoodResponse.data);
+                console.log(neighborhoodResponse.data);
+            }
+            getNeighborhood();
+        }
+    }, [neighborhoodId]);
+
     return (
-        <div>UsInfo</div>
+        <div className='nnn'>
+            { neighborhoodData? 
+                <>
+                    {neighborhoodData.logo_url ? 
+                    <img src={neighborhoodData.logo_url} width='100px' height='100px' alt="jv-logo" /> : null
+                    }
+                    <div>
+                        <h1>Junta de vecinos {neighborhoodData.name}</h1>
+                        <p>Código legal: {neighborhoodData.jv_code}</p>
+                        <p>Creada el {formatearFecha(neighborhoodData.created_at)}</p>
+                    </div>
+                    <div>
+                        <h2>Descripción</h2>
+                        <p>{neighborhoodData.description}</p>
+                    </div>
+                    <div>
+                        <h2>Directiva</h2>
+                        <p>Presidente: <strong>{neighborhoodData.president}</strong></p>
+                        <p>Secretario: <strong>{neighborhoodData.secretary}</strong></p>
+                        <p>Tesorero: <strong>{neighborhoodData.treasurer}</strong></p>
+                    </div>
+                    <div>
+                        <h2>Dirección</h2>
+                        <p>{neighborhoodData.address} /Comuna {userInfo.commune.commune_name} / Región {userInfo.region.region_name}</p>
+                    </div>
+                </>
+                :
+                <>No se pudo cargar la información</>
+            }
+        </div>
     )
 }
 
--- src/requests/Neighborhood.js
+++ src/requests/Neighborhood.js
@@ -1,21 +1,8 @@
 import { axiosClient } from "./AxiosClient";
 
-export const get_neighborhood_by_id = async (id) => {
-    try {
-        const response = await axiosClient.get('http://127.0.0.1:3000/neighborhoods/'+id);
-        console.log(response);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching role by ID:', error);
-        if (error.response) {
-            console.error('Server responded with error:', error.response.data);
-        } else if (error.request) {
-            console.error('No response received from the server');
-        } else {
-            console.error('Error setting up the request:', error.message);
-        }
-        throw error;
-    }
+export const get_neighborhood_by_id = async(id) => {
+    const response = await axiosClient.get(`neighborhoods/${id}`);
+    return response;
 };
 
 
